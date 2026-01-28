36bd470a6435ef1d52849484dd2bbd9dceaa9b2a
Stu Page <stuartjohnpage@live.com>
2026-01-28 12:20:39 -0600

    prepare for release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule DeltaQuery.MixProject do
   def project do
     [
       app: :delta_query,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
